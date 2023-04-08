3e466b1053480cce9105414ac6dc3ae3aa2d0ae9
Sarmad Rehman <sarmadr02@gmail.com>
2023-04-08 23:28:46 +0500

    Module for React State & Working with Events

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,7 +1,9 @@
-import "./Card.css";
+import React from 'react';
+
+import './Card.css';
 
 const Card = (props) => {
-  const classes = "card " + props.className;
+  const classes = 'card ' + props.className;
 
   return <div className={classes}>{props.children}</div>;
 };
